27c15709228b3db1d320af7cdd0e0781da7754a2
Jeff Kowalski <jeff.kowalski@gmail.com>
2020-11-28 16:11:54 -0800

    satisfy rubocop

--- jandy.rb
+++ jandy.rb
@@ -44,7 +44,7 @@ class Jandy < Thor
     def setup_logger
       redirect_output if options[:log]
 
-      @logger = Logger.new STDOUT
+      @logger = Logger.new $stdout
       @logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
       @logger.info 'starting'
     end
